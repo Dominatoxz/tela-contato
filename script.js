e5c1ccd04c99433fc7461dd744f49b94d1fb003b
Matheus Dominato <dominatomatheus06@gmail.com>
2026-02-05 21:27:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+    const successMessage = document.getElementById('successMessage');
+
+    function isValidEmail(email){
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    //função pra mostrar estado do campo
+    function setFieldState(field, isValid){
+        const control = field.closest('.form-control');
+
+        if (isValid){
+            control.classList.remove('error');
+            control.classList.add('success');
+        } else {
+            control.classList.remove('success');
+            control.classList.add('error');
+        }
+    }
+
+    //validar campo
+    function validateField(field){
+        const value = field.value.trim();
+        const errorElement = field.nextElementSibling; //pegar o elemento imediatamente após o campo
+
+        if (field.type === 'email'){
+            setFieldState(field, errorElement, value === "" ? false: isValidEmail(value));
+            //
+        } else{
+            setFieldState(field, errorElement, value !== "");
+        }
+    }
+    //validar automaticamente os campos assim que o user interage
+    ['name', 'email', 'subject', 'message'].forEach(id =>{
+        const field = document.getElementById(id);
+        field.addEventListener('blur', () => validateField(field));
+        field.addEventListener('input', () => validateField(field));  //valida ao sair digitando
+    })
+});
+
